6950cbfbdc7a0a11150d00c73679d8217a77f86b
Zewas <shumahershik@gmail.com>
2022-10-24 19:50:38 +0000

    add bot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
 from pytube import YouTube
 from sqlite3 import connect
+from discord.ext import commands
 import discord
 
 KEY = '$play '
@@ -12,7 +12,7 @@ async def download_from_youtube(urlToDownload):
     print('downloading...')
     yt = YouTube(urlToDownload)
     name = yt.streams.filter(
-        only_audio=True, mime_type="audio/webm").first().download('.')
+        only_audio=True, mime_type="audio/webm").first().download()
     # audio_file = open(name, "r+b").read()
 
     return name
@@ -22,27 +22,18 @@ async def download_from_youtube(urlToDownload):
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(
+    command_prefix=commands.when_mentioned_or("!"), intents=intents)
 
 
-@client.event
-async def on_ready():
-    print(f'Ready')
+@bot.command()
+async def play(ctx, link):
+    print(link)
+    music_for_men = await download_from_youtube(link)
+    vc = await ctx.author.voice.channel.connect()
+    vc.play(discord.FFmpegPCMAudio(
+        executable="/usr/bin/ffmpeg", source=music_for_men))
+    vc.source = discord.PCMVolumeTransformer(vc.source, volume=0.05)
 
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith(KEY):
-        music_for_men = await download_from_youtube(
-            'https://www.youtube.com/watch?v=lOPSaf9bFQ4&list=RDQkNA98LJ6ko&index=3&ab_channel=CrazyCater41')
-        vc = await message.author.voice.channel.connect()
-        vc.play(discord.FFmpegPCMAudio(
-            executable="/usr/bin/ffmpeg", source=music_for_men))
-        vc.source = discord.PCMVolumeTransformer(vc.source, volume=0.02)
-
-
-client.run(
+bot.run(
     'MTAzMzY3NjY3MzM0MzgyMzkwNA.Gh06Dm.IKcYJpyNw-9DwgwbLB-_UFQFJ7GCx-YB38WN8w')
