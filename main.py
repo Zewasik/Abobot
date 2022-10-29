6d11b66bf720e5402268522b7f3c072ba981a065
Zewas <shumahershik@gmail.com>
2022-10-29 11:36:02 +0000

    refactor music queue, add skip command, removed leaving channel after empty queue

--- main.py
+++ main.py
@@ -5,26 +5,50 @@ from discord.ext import commands
 import discord
 import re
 
-KEY = '$play '
-
 ffmpeg_options = {
     'options': '-bufsize 6000k',
 }
 
 
-def get_direct_url(urlToSearch: str) -> str:
-    try:
-        return YouTube(urlToSearch).streams.get_by_itag(251).url
-    except Exception as e:
-        print(e)
-        return None
+class MusicQueue:
+    def __init__(self, music_to_add):
+        self.queue = list()
+        self.add_music(music_to_add)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if len(self.queue) > 0:
+            ans = get_direct_url(self.queue.pop(0))
+            return ans
+        else:
+            raise StopIteration
+
+    def add_music(self, query):
+        m = re.search(
+            "(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)\&?", query)
+
+        self.queue.append(m.group(0) if m else search_by_query(query))
+
+
+def get_direct_url(urlToSearch: str):
+    RECONNECT_NUM = 5
+
+    for blank in range(RECONNECT_NUM):
+        try:
+            stream = YouTube(urlToSearch).streams.get_by_itag(251)
+            return {"url": stream.url, "title": stream.title}
+        except ConnectionResetError:
+            print("Попытка номер ", blank)
+
+    return None
 
 
 def search_by_query(query: str) -> str:
     s = Search(query)
-    s.results[0]
 
-    return
+    return s.results[0].watch_url
 
 
 intents = discord.Intents.default()
@@ -37,47 +61,56 @@ bot = commands.Bot(
 class customCommand(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.music_queue = dict()
+        self.queue = dict()
 
     @commands.hybrid_command()
     async def play(self, ctx: commands.Context, link_or_query):
-        vc = [vc for vc in bot.voice_clients if vc == ctx.voice_client]
-        vc = vc[0] if len(vc) > 0 else await ctx.author.voice.channel.connect()
+        print(ctx.voice_client)
+        vc = None
+        if ctx.voice_client:
+            if ctx.author.voice.channel.id != ctx.voice_client.channel.id:
+                await ctx.send(f'Бот уже занят:( Он находится в канале {ctx.author.voice.channel.name}')
+                return
+            vc = ctx.voice_client
+        else:
+            vc = await ctx.author.voice.channel.connect()
 
         id = ctx.author.guild.id
-        if id in self.music_queue:
-            self.music_queue[id].append(link_or_query)
+        if id in self.queue:
+            self.queue[id].add_music(link_or_query)
         else:
-            self.music_queue[id] = list([link_or_query])
+            self.queue[id] = MusicQueue(link_or_query)
 
-        await ctx.send("Пацаны, добавил в очередь эту песню кароче: " + link_or_query)
+        await ctx.send(f'Пацаны, добавил в очередь эту песню кароче: {link_or_query}', ephemeral=True)
 
-        print(self.music_queue)
+        print(self.queue)
         if not vc.is_playing():
-            while True:
-                if not vc.is_playing() and len(self.music_queue[id]) > 0:
-                    print("закончил играть")
-                    current_music = self.music_queue[id].pop(0)
-                    m = re.search(
-                        "(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)\&?", current_music)
+            for current_music in self.queue[id]:
+                vc.play(discord.FFmpegPCMAudio(
+                    executable="ffmpeg", source=current_music['url'], before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"), after=lambda e: print("Music played"))
+                await ctx.send(f'Сейчас проигрывается: {current_music["title"]}')
+                # vc.source = discord.PCMVolumeTransformer(vc.source, volume=1.0)
 
-                    if m == None:
-                        # await ctx.send("Брат, ссылку введи нормально")
-                        current_music = search_by_query(current_music)
-                    else:
-                        current_music = m.group(0)
-                        current_music = get_direct_url(current_music)
-                    vc.play(discord.FFmpegPCMAudio(
-                        executable="ffmpeg", source=current_music, before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"), after=lambda e: print("Music played"))
-                    # vc.source = discord.PCMVolumeTransformer(vc.source, volume=1.0)
-                if not vc.is_playing() and len(self.music_queue[id]) <= 0:
-                    await ctx.send(content="Очередь закончилась, я ухожу:(", reference=None)
-                    await vc.disconnect()
-                    break
+                while vc.is_playing():
+                    await asyncio.sleep(1)
 
-                await asyncio.sleep(5)
+            # await ctx.send(content="Очередь закончилась, я ухожу:(", reference=None)
+            # await vc.disconnect()
 
         return
+
+    @commands.hybrid_command()
+    async def skip(self, ctx: commands.Context):
+        if ctx.voice_client:
+            if ctx.author.voice.channel.id != ctx.voice_client.channel.id:
+                await ctx.send(f'Невозможно отключить бота, не находясь в канале: {ctx.author.voice.channel.name}')
+                return
+            if ctx.voice_client.is_playing():
+                ctx.voice_client.stop()
+                await ctx.send(f'Пропустил трек')
+                return
+
+            await ctx.send(f'Бот не подключен')
 
 
 @bot.event
